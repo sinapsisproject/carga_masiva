50336735e98a01aaa9627dc9d0bf87af11c49350
Diego Baeza <diego.icinf@gmail.com>
2024-08-14 22:50:05 -0400

    insert masivo

--- admin/cargar_usuarios.php
+++ admin/cargar_usuarios.php
@@ -12,21 +12,24 @@ if (isset($_FILES['excel_file']) && !empty($_FILES['excel_file']['tmp_name'])) {
     $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
 
     $usuarios_ingresados = [];
-    $usuarios_no_ingresados = [];
-    $usuarios_error= [];
+    $data = [];
 
     // Procesar los datos del Excel
     foreach ($sheetData as $row) {
         
-        $user = get_user_by('email', $row["B"]);
+        if (array_filter($row) === []) {
+            break; // Salir del bucle si toda la fila es null
+        }
+
+        $usuarios_ingresados["user"] = $row["C"];
+
+        $user = get_user_by('email', $row["C"]);
 
         if($user){
-
-            array_push($usuarios_no_ingresados , $row["B"]);
+            $usuarios_ingresados["wp"] = "Existe";
+            $usuarios_ingresados["bd"] = "No sabemos";
 
         }else{
-
-            //$nombre = explode(" " , $row["A"]); 
 
             $user_data = array(
                 'user_login'    => $row["C"], //email
@@ -41,19 +44,41 @@ if (isset($_FILES['excel_file']) && !empty($_FILES['excel_file']['tmp_name'])) {
 
             if ( ! is_wp_error( $user_id ) ) {
                 update_user_meta( $user_id, 'show_admin_bar_front', 'false' );
-                array_push($usuarios_ingresados , $row["C"]);
+
+                $usuarios_ingresados["wp"] = "Ingresado";
+            
+
+                $body = [
+                    "primer_nombre" => $row["A"],
+                    "apellido"      => $row["B"],
+                    "username"      => $row["C"],
+                    "email"         => $row["C"],
+                    "password"      => $row["D"],
+                ];
+
+                $response = RfCoreCurl::curl('/api/users/register_user_masive' , 'POST' , NULL , $body);
+
+                if($response->status == "true"){
+                    $usuarios_ingresados["bd"] = "Ingresado";
+                }else if($response->status == "false"){
+                    $usuarios_ingresados["bd"] = "Existe";
+                }
+
+                
             } else {
-                array_push($usuarios_error , $row["C"]);
+                $usuarios_ingresados["wp"] = "Error al ingresar";
+                $usuarios_ingresados["bd"] = "No sabemos";
             }
 
         }
 
+        array_push($data , $usuarios_ingresados);
+
+
     }
 
     wp_send_json(array(
-        'usuarios_ingresados' => $usuarios_ingresados,
-        'usuarios_no_ingresados' => $usuarios_no_ingresados,
-        'usuarios_error' => $usuarios_error
+        "response" => $data
     ));
 
 } else {
